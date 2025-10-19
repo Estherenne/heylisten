da5f2c21bb96613d080dd69d83d8334aef8c7cb4
Estherenne <esther@moths.lol>
2025-10-19 19:39:13 +0200

    feat: add notification system

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ use battery::{
     units::ratio::percent
 };
 
-use std::io;
-use std::thread;
-use std::time::Duration;
+use notify_rust::{
+    Notification,
+    Hint,
+    Urgency::Critical
+};
+
+use std::{
+    io,
+    thread,
+    time::Duration
+};
 
 fn main() -> battery::Result<()> {
-    let manager = battery::Manager::new()?;
+    let manager = Manager::new()?;
     let mut battery = match manager.batteries()?.next() {
         Some(Ok(battery)) => battery,
         Some(Err(e)) => {
@@ -20,12 +28,44 @@ fn main() -> battery::Result<()> {
             return Err(io::Error::from(io::ErrorKind::NotFound).into());
         }
     };
+        
+    let battery_model_text = format!("Battery {}:", battery.model().unwrap());
+    let mut sent_notification_low = false;
+    let mut sent_notification_critical = false;
+    let mut sent_notification_full = false;
 
     loop {
-        if battery.state_of_charge().get::<percent>() < 50.0 {
-            println!("battery has reached low capacity");
+        if !sent_notification_low && battery.state_of_charge().get::<percent>() <= 20.0 {
+            let _ = Notification::new()
+                .summary(&battery_model_text)
+                .body("Power level is low")
+                .show();
+            sent_notification_low = true;
+        } else if sent_notification_low && battery.state_of_charge().get::<percent>() >= 21.0 {
+            sent_notification_low = false;
         }
-        println!("{:?}", battery.state_of_charge().get::<percent>());
+
+        if !sent_notification_critical && battery.state_of_charge().get::<percent>() <= 10.0 {
+            let _ = Notification::new()
+                .summary(&battery_model_text) 
+                .body("Power level is critical")
+                .hint(Hint::Urgency(Critical))
+                .show(); 
+            sent_notification_critical = true;
+        } else if sent_notification_critical && battery.state_of_charge().get::<percent>() >= 11.0 {
+            sent_notification_critical = false;
+        }
+
+        if !sent_notification_full && battery.state_of_charge().get::<percent>() == 100.0 {
+            let _ = Notification::new()
+                .summary(&battery_model_text)
+                .body("Battery is full")
+                .show();
+            sent_notification_full = true;
+        } else if sent_notification_full && battery.state_of_charge().get::<percent>() <= 99.0 {
+            sent_notification_full = false;
+        }
+
         thread::sleep(Duration::from_secs(1));
         manager.refresh(&mut battery)?;
     }
