d8b4db2bd1f04556a81627de6f44b5d339a58caa
Estherenne <esther@moths.lol>
2025-10-19 18:30:22 +0200

    chore: get batteries and process percentage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use battery::{
+    Manager,
+    units::ratio::percent
+};
+
+use std::io;
+use std::thread;
+use std::time::Duration;
+
+fn main() -> battery::Result<()> {
+    let manager = battery::Manager::new()?;
+    let mut battery = match manager.batteries()?.next() {
+        Some(Ok(battery)) => battery,
+        Some(Err(e)) => {
+            eprintln!("Unable to access battery information");
+            return Err(e);
+        }
+        None => {
+            eprintln!("Unable to find any batteries");
+            return Err(io::Error::from(io::ErrorKind::NotFound).into());
+        }
+    };
+
+    loop {
+        if battery.state_of_charge().get::<percent>() < 50.0 {
+            println!("battery has reached low capacity");
+        }
+        println!("{:?}", battery.state_of_charge().get::<percent>());
+        thread::sleep(Duration::from_secs(1));
+        manager.refresh(&mut battery)?;
+    }
 }
