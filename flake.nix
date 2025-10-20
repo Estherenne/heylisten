80e03bd60a34feaa6ccbafc0d1da9d5a391f069c
Estherenne <esther@moths.lol>
2025-10-20 15:59:25 +0200

    feat: flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "an alternative to poweralertd";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+      in {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              libnotify
+              pkg-config
+              cargo
+              rustc
+            ];
+          };
+      }
+    );
+}
